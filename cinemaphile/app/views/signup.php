e8b0ac2070490bf2c13be09b62256b30b94b7a8b
Baraon Gallegos <Baraon Gallegos>
2016-07-11 13:37:25 -0500

    Created my own directory because the one on the master wasn't doing php artisan serve. Mine has database changes. Might work on yours, might not. Create a schema on your DB called cinemaphile.

---
+++ cinemaphile/app/views/signup.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Sign Up</title>
+</head>
+<body>
+	<div >
+		<h1>This is the sign up page!</h1>
+	</div>
+</body>
+</html>
